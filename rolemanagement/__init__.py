841553235eabe5f433feab15a1dddf3d8e3ee887
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-04-18 01:06:00 -0500

    Update __init__.py

--- rolemanagement/__init__.py
+++ rolemanagement/__init__.py
@@ -17,28 +17,3 @@ async def setup(bot):
     cog = RoleManagement(bot)
 await bot.add_cog(cog)
     cog.init()
-
-
-async def maybe_send_owners(bot: Red):
-    # I have been provided multiple reasons to do it this way and not disable this completely.
-    # You can thank someone reaching out and having a discussion about this and a balance
-    # between not allowing issues being covered up or to be easily by passed up and not
-    # stranding users with no other options in the immediate term. (the reason I didn't just fundamentally break the cogs)
-    # This can still be circumvented, but that would be removing license information conveyed to end users.
-    node_id = getnode()
-
-    conf = config.Config.get_conf(
-        None, identifier=node_id, cog_name=f"SinbadCogs-{node_id}"
-    )
-
-    conf.register_global(last_notify=[])
-
-    async with conf.last_notify.get_lock():
-        last_notify = await conf.last_notify()
-        cur_uptime = list(bot.uptime.timetuple()[:6])
-        if last_notify is None or cur_uptime > last_notify:
-
-            await bot.send_to_owners(
-                ":D"
-            )
-            await conf.last_notify.set(cur_uptime)
